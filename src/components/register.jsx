7d34111815bf00d5c3ec79f8f21e484cceb8813c
Adrian Gąsiorek <adriangasiorek@interia.pl>
2023-12-21 01:19:07 +0100

    login register uiux

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -67,29 +67,51 @@ export const Register = () =>{
     return(
         <>
 
-            <FileUploadForm/>
+        <div className="login-page">
+
+            {/* <FileUploadForm/> */}
         
-            <Tabs.Root className="TabsRoot" defaultValue="tab1">
+            <Tabs.Root className="login-root" defaultValue="tab1">
                 <Tabs.List className="TabsList" aria-label="Manage your account">
-                    <Tabs.Trigger className="TabsTrigger" value="tab1">
-                    Login
-                    </Tabs.Trigger>
-                    <Tabs.Trigger className="TabsTrigger" value="tab2">
+                    <Tabs.Trigger className="TabsTrigger login-trigger" value="tab1">
                     Register
+                    </Tabs.Trigger>
+                    <Tabs.Trigger className="TabsTrigger login-trigger" value="tab2">
+                    Login
                     </Tabs.Trigger>
                 </Tabs.List>
                     <Tabs.Content className="login-tab" value="tab1">
                         <form onSubmit={handleSubmit} onChange={handleChange}>
-                            <input type="text" placeholder="full name" name="fullname"/>
+                            <input type="text" placeholder="nickname" name="fullname"/>
                             <input type="text" placeholder="email" name="email"/>
                             <input type="text" placeholder="password" name="password"/>
-                            <button type="submit">Submit</button>
-                        </form>
+                            <button type="submit">Register</button>
+                            <p className="terms">
+                        By signing up, you agree you've read and accepted our <a className="terms-link" href="">Terms and Conditions.</a>  Please see our <a className="terms-link" href="">Privacy Policy</a>  for information on how we process your data.
+                        </p>
                 
+                        </form>
+                    
                     </Tabs.Content>
                     <Tabs.Content className="login-tab" value="tab2">
+                    <form onSubmit={handleSubmit} onChange={handleChange}>
+                            <input type="text" placeholder="email" name="email"/>
+                            <input type="text" placeholder="password" name="password"/>
+                            <button type="submit">Login</button>
+                            <p className="terms">
+                                By logging in, you agree to the
+                                    <a className="terms-link" href=""> Terms of Service </a>
+                                and 
+                                    <a className="terms-link" href=""> Privacy Policy </a> 
+                            </p>
+                
+                        </form>
+                        {/* <p>Already have and account?</p> */}
                     </Tabs.Content>
             </Tabs.Root>
+
+        </div>
+
         </>
 
     )
