6857701bc1532429cc748393f8a64bd7c3dd39fd
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-01-03 03:46:39 +0100

    responsive navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -16,6 +16,7 @@ export const Navbar = () =>{
     const API = process.env.REACT_APP_API
 
     const [foundUsers, setFoundUsers] = useState()
+    const [isNavActive, setIsNavActive] = useState(false)
 
     const logout = async()=>{
         await supabase.auth.signOut()
@@ -65,75 +66,81 @@ export const Navbar = () =>{
                 <div className="logo">
                     <img src={logo} alt="legited-logo" />   
                     <img src={monogram} alt="legited-logo" className='monogram' />   
-
                 </div>
             </Link>
+            <button onClick={() => setIsNavActive(prev => !prev)} className='burger'>
+                x
+            </button>
 
-            
-            <div placeholder='Szukaj uzytkownika' className="search-bar">
-                <input onChange={((e)=>handleUpdateFoundUsers(e.target.value))} type="text" placeholder='Szukaj uzytkownika'/>
-                {
-            foundUsers?.length > 0 && (
-                <div className="pre-list">
-                <div className="found-users">
-                    {foundUsers.map((user) => (
-                    <Link to={`/Users/${user.nickname}`}>
-                        <div className='found-user' key={user.id}>
-                            <CommentsAvatar avatar={user.avatar} nickname={user.nickname}/>
-                            <p>{user.nickname}</p>
-                            {/* <p>ID: {item.id}</p> */}
-                            {/* <button onClick={() => handleSetFoundItem(item)}>Wybierz</button> */}
-                        </div>
+            <div className={`nav-links ${isNavActive ? `` : `disabled`}`}>
+                <div placeholder='Szukaj uzytkownika' className="search-bar">
+                    <input onChange={((e)=>handleUpdateFoundUsers(e.target.value))} type="text" placeholder='Szukaj uzytkownika'/>
+                    {
+                foundUsers?.length > 0 && (
+                    <div className="pre-list">
+                    <div className="found-users">
+                        {foundUsers.map((user) => (
+                        <Link to={`/Users/${user.nickname}`}>
+                            <div className='found-user' key={user.id}>
+                                <CommentsAvatar avatar={user.avatar} nickname={user.nickname}/>
+                                <p>{user.nickname}</p>
+                                {/* <p>ID: {item.id}</p> */}
+                                {/* <button onClick={() => handleSetFoundItem(item)}>Wybierz</button> */}
+                            </div>
 
-                    </Link>
-   
-                    ))}
-                </div>
-                </div>
-            )
-            }
-
-            </div>
-
-            <div className="rest">
-            <Link to='/register'>
-                <button>
-                    Jak to działa
-                </button>
-            </Link>
-
-            <Link to='/login'>
-                <button>
-                    O nas
-                </button>
-            </Link>
-
-            {
-                isLogged ?
-                (
-                    <button onClick={logout} className='btn-register'>
-                        Wyloguj się
-                    </button>
-                )
-                :
-                (
-                    <>
-                        <Link to='/login?activeTab=login' replace={false}>
-                            <button className='btn-login'>
-                                Zaloguj się
-                            </button>
-                       </Link>
-                        <Link to='/login?activeTab=register'>
-                            <button className='btn-register' replace={false}>
-                                Zarejestruj się
-                            </button>
                         </Link>
-                    
-                    </>
+    
+                        ))}
+                    </div>
+                    </div>
                 )
+                }
 
-            }
+                </div>
+
+                <div className="rest">
+                <Link to='/register'>
+                    <button>
+                        Jak to działa
+                    </button>
+                </Link>
+
+                <Link to='/login'>
+                    <button>
+                        O nas
+                    </button>
+                </Link>
+
+                {
+                    isLogged ?
+                    (
+                        <button onClick={logout} className='btn-register'>
+                            Wyloguj się
+                        </button>
+                    )
+                    :
+                    (
+                        <>
+                            <Link to='/login?activeTab=login' replace={false}>
+                                <button className='btn-login'>
+                                    Zaloguj się
+                                </button>
+                        </Link>
+                            <Link to='/login?activeTab=register'>
+                                <button className='btn-register' replace={false}>
+                                    Zarejestruj się
+                                </button>
+                            </Link>
+                        
+                        </>
+                    )
+
+                }
+                </div>
             </div>
+
+
+   
             
         </nav>
     )
