65d17e870669a5063ec010e2f1666f95b2ca43c1
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-01-26 00:17:16 +0100

    added error codes to auth failed component

--- src/Routes/AuthFailed.jsx
+++ src/Routes/AuthFailed.jsx
@@ -1,11 +1,57 @@
+import { useSearchParams } from "react-router-dom";
+import gone from '../410.svg'
+import unauthorized from '../401.svg'
+import notFound from '../404.svg'
+
+// import unauthorized from '../410.svg'
+
+
 export const AuthFailed = (props) =>{
 
+    const [searchParams] = useSearchParams();
+
+    const error = searchParams.get("error")
 
 
-    console.log(props)
+    console.log(error)
+
     return(
         <div className="central-page">
-            Nie udało się zweryfikować
+            <div className="not-found">
+                {
+                    error === '410' ?
+
+                    <>
+                       <img src={gone} alt="gone" />
+                        <h2>
+                        Link wygasł. Zeskanuj tag NFC ponownie.
+                        </h2>
+                    </>
+                    :
+
+                    error === '401' ? 
+
+                    <>
+                        <img src={unauthorized} alt="401 unauthorized" />
+                        <h2>
+                        Nieprawidłowy klucz.
+                        </h2>
+                    </>
+
+                    :
+                    
+                    <>
+                        <img src={notFound} alt="404 not found" />
+                        <h2>
+                        Nie znaleziono strony której szukasz.
+                        </h2>
+                    </>
+
+
+                }
+        
+           
+            </div>
         </div>
     )
 }
