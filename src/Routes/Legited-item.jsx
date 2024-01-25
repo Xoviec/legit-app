016ebb85a31013c0d0571af9a203cc868c6884fa
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-01-25 19:27:12 +0100

    created function to detect successful scan

--- src/Routes/Legited-item.jsx
+++ src/Routes/Legited-item.jsx
@@ -4,6 +4,53 @@ import { useEffect, useState } from 'react';
 
 export const LegitedItem = () =>{
 
+
+    const[scanData, setScanData] = useState()
+    const[isScanSuccess, setIsScanSuccess] = useState()
+ 
+    const dupa = async () =>{
+
+        try{
+
+        let ixk_txtdelay = 0;
+
+        let ixk_textdb = "Authentication Check<br>In Progress...";
+        let ixk_failurl = "https://legited.app/auth-failed";
+        let ixk_errqs = true;
+
+        var i = new URLSearchParams(window.location.search);
+        var ixk_target = "";
+        var ixk_tbresponse = {
+            status: 'unknown_error'
+        };
+        var ixk_ts = Date.now();
+        const controller = new AbortController();
+        const options = {
+            method: 'GET',
+            signal: controller.signal
+        };
+        var ixk_curl = encodeURI(window.location.hostname + window.location.pathname);
+
+        console.log(ixk_ts)
+        console.log(i.get('ixr'))
+        console.log(ixk_curl)
+        const promise = await fetch('https://t.ixkio.com/traceback?ixc=Gnuxzv&ts=' + ixk_ts + '&ixr=' + i.get('ixr') + '&ixu=' + ixk_curl, options);
+        const promiseData = await promise.json()
+        if(promiseData.status === 'key_fail'){
+            setIsScanSuccess(false)
+        }
+        else{
+            setIsScanSuccess(true)
+        }
+        console.log(promiseData)
+
+        }catch(err){
+            console.log(err)
+        }
+
+    }
+    
+
     const API = process.env.REACT_APP_API
 
 
@@ -30,32 +77,39 @@ export const LegitedItem = () =>{
     }
 
     useEffect(()=>{
+        dupa()
         getItemData()
     }, [])
 
 
-    useEffect(() => {
-        // Tworzymy element skryptu
-        const script = document.createElement('script');
     
-        // Ustawiamy atrybuty skryptu (src, defer itp.)
-        script.src = 'https://t.ixkio.com/s/traceback.js?code=Gnuxzv';
-        script.defer = true;
+
+
+    // useEffect(() => {
+    //     // Tworzymy element skryptu
+    //     const script = document.createElement('script');
     
-        // Dodajemy skrypt do elementu head dokumentu
-        document.head.appendChild(script);
+    //     // Ustawiamy atrybuty skryptu (src, defer itp.)
+    //     script.src = 'https://t.ixkio.com/s/traceback.js?code=Gnuxzv';
+    //     script.defer = true;
     
-        // Opcjonalnie, możemy oczyścić skrypt przy odmontowywaniu komponentu
-        return () => {
-          document.head.removeChild(script);
-        };
-      }, []); // 
+    //     // Dodajemy skrypt do elementu head dokumentu
+    //     document.head.appendChild(script);
+
+    
+    //     // Opcjonalnie, możemy oczyścić skrypt przy odmontowywaniu komponentu
+    //     return () => {
+    //       document.head.removeChild(script);
+    //     };
+    //   }, []); 
     
 
     return(
         <div className="central-page">
             current owner:
             {itemData?.current_owner}
+            {isScanSuccess ? <p>Tak</p> : <p>Nie</p>}
+
         </div>
     )
 }
