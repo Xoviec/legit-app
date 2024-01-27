88a19efc2e497ae477dd6e0bb3288994f0b7047d
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-01-27 23:24:49 +0100

    deleted line of text that was causing auth certificate page bugged

--- src/Routes/Legited-item.jsx
+++ src/Routes/Legited-item.jsx
@@ -115,7 +115,6 @@ export const LegitedItem = () =>{
                 :
                 <>
 
-                Status weryfikacji
                 {isScanSuccess ? 
                 <>
                    
