51c2ec9d7ef864eb3c3ab95883629bc14e59ca49
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-01-31 22:53:02 +0100

    nfc auth settings change

--- src/Routes/Legited-item.jsx
+++ src/Routes/Legited-item.jsx
@@ -32,55 +32,60 @@ export const LegitedItem = () =>{
         const itemsData = await getItemData()
 
         try{
+            var i = new URLSearchParams(window.location.search);
 
-        let ixk_txtdelay = 0;
-
-        let ixk_failurl = "https://legited.app/auth-failed";
-        let ixk_errqs = true;
-
-        var i = new URLSearchParams(window.location.search);
-        var ixk_target = "";
-        var ixk_tbresponse = {
-            status: 'unknown_error'
-        };
-        var ixk_ts = Date.now();
-        const authDate = ixk_ts
-        setAuthDate(authDate)
-        const controller = new AbortController();
-        const options = {
-            method: 'GET',
-            signal: controller.signal
-        };
-        var ixk_curl = encodeURI(window.location.hostname + window.location.pathname);
-
-        console.log(ixk_ts)
-        console.log(i.get('ixr'))
-        console.log(ixk_curl)
-        const promise = await fetch('https://t.ixkio.com/traceback?ixc=Gnuxzv&ts=' + ixk_ts + '&ixr=' + i.get('ixr') + '&ixu=' + ixk_curl, options);
-        const promiseData = await promise.json()
-        setIsDataLoaded(true)
-        if(promiseData.status === 'key_fail'){
-            setIsScanSuccess(false)
-            setAuthError(promiseData.qserror)
-            switch(promiseData.qserror){
-                default:
-                    navigate('/auth-failed?error=400', { replace: true,  state: {itemsData, authDate}})
-                    return
-                case('expired_key'):
-                    navigate('/auth-failed?error=410', { replace: true,  state: {itemsData, authDate}})
-                    return
-                case('no_key'):
-                    navigate('/auth-failed?error=400', { replace: true,  state: {itemsData, authDate}})
-                    return
-                case('key_not_found'):
-                    navigate('/auth-failed?error=401', { replace: true,  state: {itemsData, authDate}})
-                    return
+            if((i.get('failed'))===true){
+                navigate('/auth-failed?error=400', { replace: true,  state: {itemsData, authDate}})
+                return
             }
-        }
-        else{
-            setIsScanSuccess(true)
-        }
-        console.log(promiseData)
+
+            let ixk_txtdelay = 0;
+
+            let ixk_failurl = "https://legited.app/auth-failed";
+            let ixk_errqs = true;
+
+            var ixk_target = "";
+            var ixk_tbresponse = {
+                status: 'unknown_error'
+            };
+            var ixk_ts = Date.now();
+            const authDate = ixk_ts
+            setAuthDate(authDate)
+            const controller = new AbortController();
+            const options = {
+                method: 'GET',
+                signal: controller.signal
+            };
+            var ixk_curl = encodeURI(window.location.hostname + window.location.pathname);
+
+            console.log(ixk_ts)
+            console.log(i.get('ixr'))
+            console.log(ixk_curl)
+            const promise = await fetch('https://t.ixkio.com/traceback?ixc=Gnuxzv&ts=' + ixk_ts + '&ixr=' + i.get('ixr') + '&ixu=' + ixk_curl, options);
+            const promiseData = await promise.json()
+            setIsDataLoaded(true)
+            if(promiseData.status === 'key_fail'){
+                setIsScanSuccess(false)
+                setAuthError(promiseData.qserror)
+                switch(promiseData.qserror){
+                    default:
+                        navigate('/auth-failed?error=400', { replace: true,  state: {itemsData, authDate}})
+                        return
+                    case('expired_key'):
+                        navigate('/auth-failed?error=410', { replace: true,  state: {itemsData, authDate}})
+                        return
+                    case('no_key'):
+                        navigate('/auth-failed?error=400', { replace: true,  state: {itemsData, authDate}})
+                        return
+                    case('key_not_found'):
+                        navigate('/auth-failed?error=401', { replace: true,  state: {itemsData, authDate}})
+                        return
+                }
+            }
+            else{
+                setIsScanSuccess(true)
+            }
+            console.log(promiseData)
 
         }catch(err){
             console.log(err)
