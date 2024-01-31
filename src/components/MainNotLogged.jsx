d9a32bd8d4308fb9eb7ae4ee1c89de483efa5665
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-01-31 01:35:03 +0100

    redesigned not logged user page

--- src/components/MainNotLogged.jsx
+++ src/components/MainNotLogged.jsx
@@ -1,29 +1,36 @@
 import { Link } from 'react-router-dom';
 import logo from '../Legited logo.svg'
+import iphoneScreen from '../ip14pro.png'
 
 
 export const MainNotLogged = ()=>{
 
     return(
         <div className="welcomer">
-            <h1>Legited</h1>
-            {/* <img src={logo} alt="legited-logo" />    */}
+            <div className="content">
+                <h1>Legited</h1>
+                {/* <img src={logo} alt="legited-logo" />    */}
 
-            <p>Uwierzytelnianie oraz permanentny legit check na <span>Twoje</span> sneakersy za pomocą tagów NFC.</p>
-            <p> 
+                <p>Uwierzytelnianie oraz permanentny legit check na <span>Twoje</span> sneakersy za pomocą tagów NFC.</p>
+                <p> 
 
-            <div className="buttons">
-                <button className='btn-join'>Dołącz</button>
-                <button className='btn-how'>Jak to działa?</button>
-            </div>
-  
+                <div className="buttons">
+                    <button className='btn-join'>Dołącz</button>
+                    <button className='btn-how'>Jak to działa?</button>
+                </div>
+    
 
-            {/* <Link to='/register'>
-                <span>Dołącz </span>  
-            </Link> */}
+                {/* <Link to='/register'>
+                    <span>Dołącz </span>  
+                </Link> */}
 
-                {/* aby wykorzystać wszystkie funkcje! */}
+                    {/* aby wykorzystać wszystkie funkcje! */}
                 </p>
+            </div>
+            <div className="image-container">
+                <img src={iphoneScreen} alt="" />
+            </div>
+    
         </div>
     )
 }
