c7d4c0f7fbd37f11654f859ed878c750606efc85
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-02-16 21:40:56 +0100

    refactored main user page with react query

--- src/components/MainPage/Main.jsx
+++ src/components/MainPage/Main.jsx
@@ -53,16 +53,71 @@ const path = location.pathname
   } 
 
 
-  const getUserItems = async () =>{
-
-    try{
-      const userItemsListResponse = await fetch(`${API}/user-items/${nickNameFromLocalStorage}`);
-      const userItemsData = await userItemsListResponse.json()
-      setUserItemsList(userItemsData)
-    }catch(userItemsError){
-      console.log(userItemsError)
-    }
+  const getItems = async () =>{
+    return await fetch(`${API}/user-items/${nickNameFromLocalStorage}`)
+      .then(res=>res.json())
   }
+
+  const {
+    status: itemsStatus,
+    error: itemsError,
+    data: itemsData,
+  } = useQuery({
+    queryKey: ['items',nickNameFromLocalStorage],
+    queryFn: getItems,
+  })
+
+  const getComments = async () =>{
+    const { data: { user } } = await supabase.auth.getUser()
+
+    return await fetch(`${API}/get-comments/${user.id}`)
+      .then(res=>res.json())
+  }
+
+  const {
+    status: commentsStatus,
+    error: commentsError,
+    data: commentsData,
+  } = useQuery({
+    queryKey: ['comments',nickNameFromLocalStorage],
+    queryFn: getComments,
+  })
+
+
+  const getProfileData = async () =>{
+    const { data: { user } } = await supabase.auth.getUser()
+    const { data: {session}, error } = await supabase.auth.getSession()
+
+
+
+    return await fetch(`${API}/secret/${user.id}`, {
+      method: 'GET',
+      headers: {
+        'jwt': (session.access_token),
+      }
+    })
+    .then(res=>res.json())
+    .then(res=>res[0])
+  }
+
+  const {
+    status: profileDataStatus,
+    error: profileDataError,
+    data: profileDataData,
+  } = useQuery({
+    queryKey: ['profileData',nickNameFromLocalStorage],
+    queryFn: getProfileData,
+  })
+  // const getUserItems = async () =>{
+
+  //   try{
+  //     const userItemsListResponse = await fetch(`${API}/user-items/${nickNameFromLocalStorage}`);
+  //     const userItemsData = await userItemsListResponse.json()
+  //     setUserItemsList(userItemsData)
+  //   }catch(userItemsError){
+  //     console.log(userItemsError)
+  //   }
+  // }
 
 
   // const {
@@ -97,17 +152,17 @@ const path = location.pathname
       }
 
 
-      try{
-        const commentsRes = await fetch(`${API}/get-comments/${user.id}`)
-        const commentsData = await commentsRes.json()
-        setComments(commentsData)
-      }catch(commentsError){
-        console.log(commentsError)
-      }
+      // try{
+      //   const commentsRes = await fetch(`${API}/get-comments/${user.id}`)
+      //   const commentsData = await commentsRes.json()
+      //   setComments(commentsData)
+      // }catch(commentsError){
+      //   console.log(commentsError)
+      // }
     };
 
     fetchData()
-    getUserItems()
+    // getUserItems()
     getMostItems()
   }, [path]);
   
@@ -120,17 +175,17 @@ const path = location.pathname
               
             <div className="profile-container">
             <div className="user-info">
-                <MyAvatar user={publicUser}/>
-                <h1>{publicUser?.nickname || <Skeleton width={200}/>}</h1>
-                <Link state={publicUser}to={`/settings`} className='settings-btn'>
+                <MyAvatar user={profileDataData}/>
+                <h1>{profileDataData?.nickname || <Skeleton width={200}/>}</h1>
+                <Link state={profileDataData}to={`/settings`} className='settings-btn'>
                   <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.8536 1.14645C11.6583 0.951184 11.3417 0.951184 11.1465 1.14645L3.71455 8.57836C3.62459 8.66832 3.55263 8.77461 3.50251 8.89155L2.04044 12.303C1.9599 12.491 2.00189 12.709 2.14646 12.8536C2.29103 12.9981 2.50905 13.0401 2.69697 12.9596L6.10847 11.4975C6.2254 11.4474 6.3317 11.3754 6.42166 11.2855L13.8536 3.85355C14.0488 3.65829 14.0488 3.34171 13.8536 3.14645L11.8536 1.14645ZM4.42166 9.28547L11.5 2.20711L12.7929 3.5L5.71455 10.5784L4.21924 11.2192L3.78081 10.7808L4.42166 9.28547Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
                   <span className='settings-text'>Edytuj profil</span>
                 </Link>
             </div>
             {
-              publicUser?.description && <><p className='user-about'>O mnie:</p> <p>{publicUser?.description}</p></>
+              profileDataData?.description && <><p className='user-about'>O mnie:</p> <p>{profileDataData?.description}</p></>
             }
-            <ProfileTabs userItemsList={userItemsList} comments={comments}/>
+            <ProfileTabs userItemsList={itemsData} comments={commentsData}/>
           </div> 
             )
 
