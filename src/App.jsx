5b004357e2e8893d072e4cd28c1f69e78d5ac355
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-02-19 03:34:50 +0100

    ios safari fix try

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { supabase } from './components/supabaseClient';
-import { User } from '@supabase/supabase-js';
-import { Register } from './components/Register/register';
-import { Login } from './components/Register/login';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { Script } from './script';
 import { MainNotLogged } from './components/MainPageNotLoggedd/MainNotLogged';
 import { NavbarSimple } from './components/Layout/NavbarSimple/NavbarSimple';
 import { SoonText } from './components/MainPageNotLoggedd/SoonText';
