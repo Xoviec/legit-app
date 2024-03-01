884fff3e567804789c15a9d331fa52b043de28a9
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-03-02 00:04:04 +0100

    verify page design

--- src/Components/Register/WaitingForVerify.jsx
+++ src/Components/Register/WaitingForVerify.jsx
@@ -1,11 +1,16 @@
 
 import './WaitingForVerify.css'
+import mailed from '../../assets/mailed.svg'
+
 
 export const WaitingForVerify = () =>{
 
     return(
         <div className="login-page">
             <div className="verify-container">
+                <img src={mailed} alt="mail icon" />
+                <h1>Zweryfikuj swój email</h1>
+                <p>Na adres <span className='email-address'> &#123;addres&#125;</span> został przesłany link weryfikacyjny w celu dokończenia rejestracji. Logowanie będzie możliwe po jego otwarciu. </p>
             </div>
         </div>
     )
