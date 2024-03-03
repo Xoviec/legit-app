d82aadf5f6f1228f05fedfa663010e0e17027277
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-03-03 23:25:56 +0100

    updated helmet meta tags

--- src/Components/Helmet/HelmetSpecified.jsx
+++ src/Components/Helmet/HelmetSpecified.jsx
@@ -6,14 +6,16 @@ export const HelmetSpecified = ({nickname, desc}) =>{
 
        <Helmet>
             <title>{nickname}</title>
-            <meta name={desc} content="Uwierzytelnianie oraz permanentny legit check na Twoje sneakersy za pomocą tagów NFC" />
+            <meta name="description" content={desc} />
             <meta name="keywords" content="react, meta tags, seo, streetwear,yeezy,nike,adidas,jordan,jordan low,low,dunk,dunk low,y2k,supreme,legit,legited,lc,LC,legit,legitcheck,NFC" />
             <meta name="author" content={nickname} />
-            <meta property="og:title" content="Legited" />
+            <meta property="og:title" content={nickname} />
             <meta property="og:image" content="../../assets/monogram.svg" />
-            <meta property="og:url" content="https://legited.app" />
-            <meta name="twitter:title" content="Legited" />
-            <meta name="twitter:description" content="Uwierzytelnianie oraz permanentny legit check na Twoje sneakersy za pomocą tagów NFC" />
+            <meta property="og:url" content={`https://legited.app/Users/${nickname}`} />
+            <meta property="og:description" content={desc} />
+            <meta property='profile:username' content={nickname}/>
+            <meta name="twitter:title" content={nickname} />
+            <meta name="twitter:description" content={desc}/>
             <meta name="twitter:image" content="../../assets/monogram.svg" />
             <meta name="twitter:card" content="summary_large_image" />
        </Helmet>
