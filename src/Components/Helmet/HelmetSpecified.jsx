279762513256cc5cf77cfc7502f5ecad803ed378
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-03-08 21:05:54 +0100

    fixed verification image position

--- src/Components/Helmet/HelmetSpecified.jsx
+++ src/Components/Helmet/HelmetSpecified.jsx
@@ -4,25 +4,22 @@ export const HelmetSpecified = ({nickname, desc}) =>{
 
     return(
         <HelmetProvider>
-
-
-       <Helmet>
-            <title>{nickname}</title>
-            <meta name="description" content={desc} />
-            <meta name="keywords" content="react, meta tags, seo, streetwear,yeezy,nike,adidas,jordan,jordan low,low,dunk,dunk low,y2k,supreme,legit,legited,lc,LC,legit,legitcheck,NFC" />
-            <meta name="author" content={nickname} />
-            <meta property="og:title" content={nickname} />
-            <meta property="og:image" content="../../assets/monogram.svg" />
-            <meta property="og:url" content={`https://legited.app/Users/${nickname}`} />
-            <meta property="og:description" content={desc} />
-            <meta property='og:site_name' content='Legited'/>
-            <meta property='profile:username' content={nickname}/>
-            <meta name="twitter:title" content={nickname} />
-            <meta name="twitter:description" content={desc}/>
-            <meta name="twitter:image" content="../../assets/monogram.svg" />
-            <meta name="twitter:card" content="summary_large_image" />
-       </Helmet>
-
+            <Helmet>
+                <title>{nickname}</title>
+                <meta name="description" content={desc} />
+                <meta name="keywords" content="react, meta tags, seo, streetwear,yeezy,nike,adidas,jordan,jordan low,low,dunk,dunk low,y2k,supreme,legit,legited,lc,LC,legit,legitcheck,NFC" />
+                <meta name="author" content={nickname} />
+                <meta property="og:title" content={nickname} />
+                <meta property="og:image" content="../../assets/monogram.svg" />
+                <meta property="og:url" content={`https://legited.app/Users/${nickname}`} />
+                <meta property="og:description" content={desc} />
+                <meta property='og:site_name' content='Legited'/>
+                <meta property='profile:username' content={nickname}/>
+                <meta name="twitter:title" content={nickname} />
+                <meta name="twitter:description" content={desc}/>
+                <meta name="twitter:image" content="../../assets/monogram.svg" />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Helmet>
        </HelmetProvider>
 
     )
