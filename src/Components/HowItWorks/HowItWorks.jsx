a19961b58588e26c51ae20c57d925f7c22b9e954
Adrian Gąsiorek <adriangasiorek@interia.pl>
2024-03-26 17:48:00 +0100

    responsive how it works component

--- src/Components/HowItWorks/HowItWorks.jsx
+++ src/Components/HowItWorks/HowItWorks.jsx
@@ -18,11 +18,11 @@ export const HowItWorks = () =>{
         <div className="how-it-works-page">
             <h1>Jak to działa?</h1>
             <div className="steps-container">
-                <StepCard number={1} title={'Weryfikacja'} desc={'Otrzymany przez nas przedmiot podlega weryfikacji autentyczności'} icon={verify}/>
-                <StepCard number={2} title={'Montaż'} desc={'Do pomyślnie zweryfikowanego przedmiotu zostaje zamontowany tag NFC umożliwiający jego weryfiikacje'} icon={montage}/>
-                <StepCard number={3} title={'Rejestracja'} desc={'Gotowy przedmiot oraz tag NFC zostają przypisane do Twojego konta, możesz nimi zarządzać na stronie'} icon={assign}/>
+                <StepCard number={1} title={'Weryfikacja'} desc={'Otrzymany przez nas przedmiot podlega weryfikacji autentyczności.'} icon={verify}/>
+                <StepCard number={2} title={'Montaż'} desc={'Do pomyślnie zweryfikowanego przedmiotu zostaje zamontowany tag NFC umożliwiający jego weryfiikacje.'} icon={montage}/>
+                <StepCard number={3} title={'Rejestracja'} desc={'Gotowy przedmiot oraz tag NFC zostają przypisane do Twojego konta. Możesz nimi zarządzać na stronie.'} icon={assign}/>
             </div>
-            <h2>Co dalej?</h2>
+            {/* <h2>Co dalej?</h2> */}
     
         </div>
     )
